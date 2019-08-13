fb81aad48a98c277e69a02edbc85117ba9b2f929
Alexander <cavinc20@gmail.com>
2019-08-13 16:52:14 +0700

    Добавлена кнопка назад в режиме сканирования

--- app/src/main/java/com/cav/invetnar/ui/fragments/ScannerFragment.java
+++ app/src/main/java/com/cav/invetnar/ui/fragments/ScannerFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.util.TypedValue;
@@ -115,8 +116,16 @@ public class ScannerFragment extends Fragment implements View.OnClickListener{
         }
 
         ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Сканирование");
+        setTools();
 
         return view;
+    }
+
+    private void setTools(){
+        ActionBar actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 
     @Override
@@ -149,6 +158,9 @@ public class ScannerFragment extends Fragment implements View.OnClickListener{
         }
         if (item.getItemId() == R.id.scanned_close) {
             storeClose();
+        }
+        if (item.getItemId() == android.R.id.home) {
+            getActivity().onBackPressed();
         }
         return super.onOptionsItemSelected(item);
     }
